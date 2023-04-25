a728b5b3ef9e9286d3d8567275743f2e3b2c04d9
Inaldo Junior <inaldoanjosjr@gmail.com>
2023-04-24 21:32:54 -0300

    EC6 Modules - Import, Export

---
+++ Stage_05/EC6/main.js
@@ -0,0 +1,5 @@
+import message from './utils.js' // nessa linha ela importa e já executa o arquivo
+// lembrando que esse 'message' pode ser qualquer nome, por exemplo:
+// import qualquernome from './utils.js'
+
+window.console.log(message);
---
+++ Stage_05/EC6/utils.js
@@ -0,0 +1,4 @@
+// utils.js
+const message = "Minha mensagem"; // essa variável fica isolada nesse arquivo, e não consigo usar ela em outro lugar
+
+export default message;
