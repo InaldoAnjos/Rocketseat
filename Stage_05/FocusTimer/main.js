511833484e04ff04ba641f8d389655ade1e09b56
Inaldo Junior <inaldoanjosjr@gmail.com>
2023-05-03 14:32:57 -0300

    focustimer

---
+++ Stage_05/FocusTimer/main.js
@@ -0,0 +1,45 @@
+// Variáveis
+
+const btnPlay     = document.querySelector('.play');
+const btnPause    = document.querySelector('.pause');
+const btnSet      = document.querySelector('.set');
+const btnStop     = document.querySelector('.stop');
+const btnSoundOn  = document.querySelector('.sound-on');
+const btnSoundOff = document.querySelector('.sound-off');
+
+let   minutes;
+
+// Events - DOM (Event-driven)
+
+btnPlay.addEventListener('click', function() {
+    btnPlay.classList.add('hide');
+    btnPause.classList.remove('hide');
+    btnSet.classList.add('hide');
+    btnStop.classList.remove('hide');
+});
+
+btnPause.addEventListener('click', function() {
+    btnPause.classList.add('hide');
+    btnPlay.classList.remove('hide');
+});
+
+btnSoundOn.addEventListener('click', function() {
+    btnSoundOn.classList.add('hide');
+    btnSoundOff.classList.remove('hide');
+});
+
+btnSoundOff.addEventListener('click', function() {
+    btnSoundOff.classList.add('hide');
+    btnSoundOn.classList.remove('hide');
+});
+
+btnStop.addEventListener('click', function() {
+    btnPlay.classList.remove('hide');
+    btnPause.classList.add('hide');
+    btnStop.classList.add('hide');
+    btnSet.classList.remove('hide');
+});
+
+btnSet.addEventListener('click', function() {
+    minutes = prompt('Quantos minutos?');
+});
